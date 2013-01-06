9179b6e20b2d43023b537220488279487f9303ed
JunhuaGu <junhuagu043@163.com>
2013-01-07 00:13:32 +0800

    jjjj

---
+++ src/jjjj.java
@@ -0,0 +1,22 @@
+
+
+
+public class jjjj {
+	public int id;
+	public String name;
+	
+	public void ghgg(){
+		
+	}
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	
+	/**
+	 * fuck u  github.com
+	 */
+	
+	//Long is a  Genius !
+	//Long is a  Genius !
+}
